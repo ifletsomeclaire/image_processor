e14f78ddd67191017f48d3a36971da489a5b79e6
ifletsomeclaire <69061963+ifletsomeclaire@users.noreply.github.com>
2020-10-17 14:44:44 -0500

    move upscale functions to main processor

--- processor/src/main.rs
+++ processor/src/main.rs
@@ -3,15 +3,21 @@ use upscale::upscale_images;
 use utils::{generate_gif, save_images, walkdir_for_images};
 
 mod utils;
+mod upscale;
 
 fn main() {
+    read_sample_folder_and_dain_it();
+}
+
+// saving this for reference material :)
+fn read_sample_folder_and_dain_it() {
     let root = std::env::current_dir().unwrap();
     let width = 200;
     let height = 200;
 
     let mut images = walkdir_for_images("image_samples/stars");
     upscale_images(&mut images, width, height);
-    save_images("x_output/stars", &images);
+    save_images("x_output/image_files", &images);
 
     let gif_path = "x_output/gif_hack.gif";
     generate_gif(gif_path, images, width, height);
